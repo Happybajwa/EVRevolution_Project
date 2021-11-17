ff1a7dda6c694f5baae5c529dcae10f69f735574
HappyBajwa <happybajwa100@gmail.com>
2021-11-17 23:11:33 +1300

    saving chnages

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,6 +1,7 @@
 window.onresize = checkScreenSize2
 window.onload = checkScreenSize
-
+//function for loading Navbar
+//Also on screen resize
 function loadMenu()
 {
     let xmlhttp = new XMLHttpRequest();
@@ -9,6 +10,7 @@ function loadMenu()
     xmlhttp.send();
     oe.innerHTML = xmlhttp.responseText;
 }
+//This funtion is for loading reuseable hamburger menu
 function hamburgerMenu()
 {
     let xmlhttp = new XMLHttpRequest();
@@ -17,7 +19,7 @@ function hamburgerMenu()
     xmlhttp.send();
     me.innerHTML = xmlhttp.responseText;
 }
-
+//this function will check screen size and call the init() function to call canvas animation
 function checkScreenSize()
 {
    checkScreenSize2()
@@ -37,10 +39,11 @@ function checkScreenSize2()
         }
 
 }
-
+//Canvas animation code
 var can, ctx, step = 10, steps = 50;
 delay = 20;
-function init() {
+function init()
+{
 
     step = 10,
     steps = 30;
@@ -54,7 +57,9 @@ ctx.textAlign = "center";
 ctx.textBaseline = "middle";
 TextSmallToBig();
 }
-function TextSmallToBig() {
+//function for making animation text from small to big
+function TextSmallToBig()
+{
 step++;
 ctx.clearRect(0, 0, can.width, can.height);
 ctx.save();
@@ -65,3 +70,59 @@ ctx.restore();
 if (step < steps)
     var t = setTimeout('TextSmallToBig()', 30);
 }
+//reusable footer code
+class Footer extends HTMLElement {
+    connectedCallback() 
+    {
+      this.innerHTML = `    
+        <footer>            
+        <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download our App</h3>
+                    <p>Download App for Android and ios mobile phone.</p>
+                    <div class="app-logo">
+                        <a href="https://play.google.com/store" target="blank"><img src="/All-images/HomePage-Images/androidlogo.png"></a>
+                         <a href="https://www.apple.com/nz/app-store/" target="blank"><img src="/All-images/HomePage-Images/IOS-APP.svg"></a>
+                 </div>
+                </div>
+                <div class="footer-col-2">
+                 <img src="/All-images/HomePage-Images/footerLOGO.png" alt="">
+                 <ul>
+                     <li><a href="/HTML/home.html">Home</a></li>
+                     <li><a href="/HTML/whyev.html">Why EV</a></li>
+                     <li><a href="/HTML/petrol&Diesel.html">Petrol & Diesle</a></li>
+                     <li><a href="/HTML/climate-impact.html">Climate Impact</a></li>
+                     <li><a href="/HTML/News.html">News</a></li>
+                     <li><a href="/HTML/aboutus.html">About us</a></li>
+                 </ul>
+             </div>
+             <div class="footer-col-3">
+                 <h3>Useful links</h3>
+                 <ul>
+                     <li><a href="https://driveelectric.org.nz/individuals/ev-models-and-where-to-buy/" target="blank">Electric Vehicles</a></li>
+                     <li><a href="https://www.nzta.govt.nz/vehicles/clean-car-programme/clean-car-discount/" target="blank">Goverment Rebate</a></li>
+                     <li><a href="https://genless.govt.nz/for-everyone/on-the-move/consider-electric-vehicles/why-buy-an-ev/?gclid=CjwKCAjwiY6MBhBqEiwARFSCPvXzLgKRGMYUXRllaV7OZaZcixvj_Me2bMCu1vg-0P8pDXwReReEnhoC_p0QAvD_BwE" target="blank">EV Benifits</a></li>
+                     <li><a href="https://insideevs.com/car-lists/electric-trucks/" target="blank">Electric Trucks</a></li>
+                 </ul>
+             </div>
+             <div class="footer-col-4">
+                 <h3>Follow us</h3>
+                 <ul>
+                     <li><a href="https://www.facebook.com/" target="blank">Facebook</a></li>
+                     <li><a href="https://twitter.com/?lang=en" target="blank">Twiter</a></li>
+                     <li><a href="https://www.instagram.com/" target="blank">Instagram</a></li>
+                     <li><a href="https://www.youtube.com/" target="blank">Youtube</a></li>
+                 </ul>
+             </div>
+            </div>
+            <hr>
+            <p class="copyright">&#9400; Copyright 2021-This is Student Project by Harpreet Singh</p>
+        </div>
+    </div>      
+        </footer>     
+      `;
+    }
+  }
+  customElements.define('main-footer', Footer);
