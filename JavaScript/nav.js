1ca6af4a7cb95f206a2e7329d80dbfd294c5b6f9
HappyBajwa <happybajwa100@gmail.com>
2021-11-17 14:17:52 +1300

    news page done

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,4 +1,4 @@
-window.onresize = checkScreenSize
+window.onresize = checkScreenSize2
 window.onload = checkScreenSize
 
 function loadMenu()
@@ -20,6 +20,12 @@ function hamburgerMenu()
 
 function checkScreenSize()
 {
+   checkScreenSize2()
+        init()
+}
+
+function checkScreenSize2()
+{
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     if(width <= 990)
         {   
@@ -29,5 +35,33 @@ function checkScreenSize()
         {
             loadMenu();
         }
+
 }
 
+var can, ctx, step = 10, steps = 50;
+delay = 20;
+function init() {
+
+    step = 10,
+    steps = 30;
+    delay = 20;
+
+can = document.getElementById("MyCanvas");
+ctx = can.getContext("2d");
+ctx.fillStyle = "#5ea31d";
+ctx.font = "6pt Poppins";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+TextSmallToBig();
+}
+function TextSmallToBig() {
+step++;
+ctx.clearRect(0, 0, can.width, can.height);
+ctx.save();
+ctx.translate(can.width / 2, can.height / 2);
+ctx.font = step + "pt Helvetica";
+ctx.fillText("WELCOME TO EV REVOLUTION NEWS", 0, 0);
+ctx.restore();
+if (step < steps)
+    var t = setTimeout('TextSmallToBig()', 30);
+}
